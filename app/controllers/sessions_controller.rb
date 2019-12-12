32def4ceeb2e6c3e8eb93f9299e3aa316252df2a
lemon-xhau <lemonn.xhau@gmail.com>
2019-12-12 14:06:02 +0700

    Chapter 10 : updating, showing and deleting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       flash[:success] = t ".welcome"
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = t ".invalid"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.select(:id, :name, :email, :is_admin).order(:name)
+            .page(params[:page]).per Settings.user.index.per_page
+  end
+
   def new
     @user = User.new
   end
@@ -19,10 +27,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".update"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".error"
+      render :edit
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = t ".login_first"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
